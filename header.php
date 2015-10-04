8332fc4c7cfd20ed72d6deac41e4c5560a5e37dc
Robert DeVore <deviodigital@gmail.com>
2015-10-03 21:30:54 -0400

    Added translation for toggle navigation text

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
+                    <span class="sr-only"><?php esc_html_e( 'Toggle Navigation', 'cleanblog' ); ?></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
