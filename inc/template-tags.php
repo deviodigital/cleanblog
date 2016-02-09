d8cd71d5424d2c759f597499396ef376f3841f7f
Robert DeVore <deviodigital@gmail.com>
2016-02-09 08:18:01 -0500

    Updating code structure

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -276,7 +276,7 @@ if ( ! function_exists( 'cleanblog_header' ) ) :
  */
 function cleanblog_header() { ?>
 
-	<?php if(is_single()) { ?>
+	<?php if( is_single() ) { ?>
 	
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
@@ -301,7 +301,7 @@ function cleanblog_header() { ?>
 	
 	<?php } elseif ( is_page_template( 'page-builder.php' ) ) { ?>
 
-	<?php } elseif(is_page()) { ?>
+	<?php } elseif ( is_page() ) { ?>
 	
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
@@ -328,7 +328,7 @@ function cleanblog_header() { ?>
 		<!-- /.container -->
     </header>
 	
-	<?php } elseif(is_search()) { ?>
+	<?php } elseif( is_search() ) { ?>
 	
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
@@ -354,7 +354,7 @@ function cleanblog_header() { ?>
 	
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-	<?php if (get_theme_mod('cleanblog_homeintro_image') !='') { ?>
+	<?php if ( get_theme_mod('cleanblog_homeintro_image') !='' ) { ?>
 	<?php $headerimg = get_theme_mod( 'cleanblog_homeintro_image' ); ?>
 	<?php } else { ?>
 	<?php $headerimg = get_template_directory_uri() . '/img/home-bg.jpg'; ?>
@@ -364,7 +364,7 @@ function cleanblog_header() { ?>
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-			<?php if (get_theme_mod('cleanblog_homeintro_title') !='') { ?>
+			<?php if ( get_theme_mod('cleanblog_homeintro_title') !='' ) { ?>
 			<h1 class="homeintro"><?php echo get_theme_mod( 'cleanblog_homeintro_title' ); ?></h1>
 			<?php } else { ?>
                         <h1><?php esc_html_e( 'Clean Blog', 'cleanblog' ); ?></h1>
@@ -397,7 +397,7 @@ if ( ! function_exists( 'cleanblog_social' ) ) :
 function cleanblog_social() { ?>
 
 	<ul class="list-inline text-center">
-		<?php if (get_theme_mod( 'cleanblog_social_twitter' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_twitter' ) !='' ) { ?>
 		<li id="social-twitter">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_twitter' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -407,7 +407,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_facebook' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_facebook' ) !='' ) { ?>
 		<li id="social-facebook">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_facebook' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -417,7 +417,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_google' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_google' ) !='' ) { ?>
 		<li id="social-google">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_google' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -427,7 +427,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_pinterest' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_pinterest' ) !='' ) { ?>
 		<li id="social-pinterest">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_pinterest' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -437,7 +437,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_linkedin' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_linkedin' ) !='' ) { ?>
 		<li id="social-linkedin">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_linkedin' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -447,7 +447,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_github' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_github' ) !='' ) { ?>
 		<li id="social-github">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_github' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -457,7 +457,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_instagram' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_instagram' ) !='' ) { ?>
 		<li id="social-instagram">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_instagram' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -467,7 +467,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_medium' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_medium' ) !='' ) { ?>
 		<li id="social-medium">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_medium' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -477,7 +477,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_vine' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_vine' ) !='' ) { ?>
 		<li id="social-vine">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_vine' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -487,7 +487,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_tumblr' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_tumblr' ) !='' ) { ?>
 		<li id="social-tumblr">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_tumblr' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
@@ -497,7 +497,7 @@ function cleanblog_social() { ?>
 			</a>
 		</li>
 		<?php } ?>
-		<?php if (get_theme_mod( 'cleanblog_social_youtbe' ) !='') { ?>
+		<?php if ( get_theme_mod( 'cleanblog_social_youtbe' ) !='' ) { ?>
 		<li id="social-youtube">
 			<a href="<?php echo get_theme_mod( 'cleanblog_social_youtube' ); ?>" target="_blank">
 				<span class="fa-stack fa-lg">
